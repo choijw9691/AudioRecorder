adfcf5af366d5b0fa934cfb2f59c6ed41dafbe0b
신현아 <ssin@gemvaxlink.com>
2021-03-18 11:39:57 +0900

    [20210108] 상용 배포건 커밋 ( v.3.142 ) 1.viewport width 보다 큰 element width 리사이징 로직 예외처리(#18343)

--- viewersdk/src/main/assets/fixedlayout_script.js
+++ viewersdk/src/main/assets/fixedlayout_script.js
@@ -2347,15 +2347,15 @@ function removeCommentNode(){
 /***************************************************** e : new custom selection */
 
 function resizeWidth(){
-
     var viewportmeta = document.querySelector('meta[name="viewport"]');
     var check = viewportmeta.content.match(/width=[^,]+/);
     var viewportWidth  = check[0].match(/\d/g);
     viewportWidth = viewportWidth.join("");
-
     $('*').each(function() {
         if($(this).outerWidth()>viewportWidth){
             var diffWidth = $(this).outerWidth() - viewportWidth;
+            if(viewportWidth-diffWidth == 0)
+                continue;
             $(this).css('width', (viewportWidth-diffWidth));
         }
     });
